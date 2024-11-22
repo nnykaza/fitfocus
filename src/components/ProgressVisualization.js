d596e5d8c52f5cb7eb118c6711375651af65633b
nnykaza <nataliabnykaza@lewisu.edu>
2024-11-22 07:46:54 -0600

    Remove unused imports to fix ESLint warnings

--- src/components/ProgressVisualization.js
+++ src/components/ProgressVisualization.js
@@ -1,8 +1,6 @@
 // src/components/ProgressVisualization.js
 
-import React, { useEffect } from 'react';
 import { Line } from 'react-chartjs-2';
-import { Chart as ChartJS } from 'chart.js/auto';
 
 const ProgressVisualization = () => {
   const data = {
